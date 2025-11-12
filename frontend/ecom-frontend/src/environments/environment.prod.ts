97385c0cf6cecf0cbf2527c23bed398b029f0c90
Pawan Talekar <ptalekar@synoptek.com>
2025-11-12 22:00:07 +0530

    Remove unintended frontend files from backend branch

--- frontend/ecom-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl: 'https://your-production-gateway-url/api'
-};
--- frontend/ecom-frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    
-    apiBaseUrl: 'https://localhost:7024/api'
-};
